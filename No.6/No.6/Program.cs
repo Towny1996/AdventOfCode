097ddecba8a8cd6b65a2060f42b2e95fc4645e2a
Arron Townshend <arron.townshend@accesspay.com>
2022-12-06 10:13:56 +0000

    AdventOfCode2022 No.6

---
+++ No.6/No.6/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace No._6
+{
+    internal class Program
+    {
+        private static Queue<char> marker = new Queue<char>();
+
+        static void Main(string[] args)
+        {
+            var line = File.ReadAllText("Input.txt");
+
+            for (var i = 0; i < line.Length; i++)
+            {
+                marker.Enqueue(line[i]);
+
+                if (marker.Count == 14 && marker.Distinct().Count() == 14)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                } 
+                else if (marker.Count == 14 && marker.Distinct().Count() != 14)
+                {
+                    marker.Dequeue();
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
