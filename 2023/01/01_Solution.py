25a07c26d14168d324625cce7777a332d8045601
arron.townshend@hotmail.co.uk <arron.townshend@hotmail.co.uk>
2023-12-02 18:16:03 +0000

    feat(01-12-2023): Day 1 Part 2 solution

--- 2023/01/01_Solution.py
+++ 2023/01/01_Solution.py
@@ -1,8 +1,6 @@
 import string
-import re
 
 class PartOne:
-
     def DoWork(self):
         input_file = open('Input.txt', 'r')
         return self.CalcCalibrationValues(input_file.readlines())
@@ -16,12 +14,37 @@ class PartOne:
         return sum(lineValues)
 
 class PartTwo:
-    number_mapper_dict = { 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine' }
+    number_mapper_dict = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 }
 
     def DoWork(self):
         input_file = open('Input.txt', 'r')
         return self.CalcCalibrationValues(input_file.readlines())
 
+    def CalcCalibrationValues(self, lines: list[string]) -> int:
+        lineValues = []
+        for l in lines:
+            ints = self.RecursiveTextDigitParse(l, 0, 1, [])
+            if(any(ints)): lineValues.append(int(ints[0] + '' + ints[-1]))
+            
+        return sum(lineValues)
         
 
-print(PartOne().DoWork())
+    def RecursiveTextDigitParse(self, text: string, start: int, end: int, lineints: []) -> []:
+        for index, char in enumerate(text[start:end]):
+            if(char.isdigit()):
+                lineints.append(char)
+                start = ((index + 1) + start)
+            else:
+                for key in self.number_mapper_dict:
+                    if text[start:end].find(key) != -1:
+                        lineints.append(str(self.number_mapper_dict[key]))
+                        start = (index + (end - 1))
+
+        if(end == len(text)): return lineints
+        else: return self.RecursiveTextDigitParse(text, start, (end + 1), lineints)
+
+
+                
+                
+
+print(PartTwo().DoWork())
