5e8dee545e6d10a4af5d04cc36c6135726202597
arron.townshend@hotmail.co.uk <arron.townshend@hotmail.co.uk>
2023-12-04 20:33:22 +0000

    feat(03-12-2023): Day 3 Part 2 Solution

--- 2023/03/03_Solution.py
+++ 2023/03/03_Solution.py
@@ -1,5 +1,6 @@
 import string
 import re
+import math
 
 class PartOne:
     special_chars = ['*', '&', '#', '/', '+', '$', '%', '@', '-', '=']
@@ -29,4 +30,35 @@ class PartOne:
 
         return sum([sum(x) for x in close_nums])
 
+class PartTwo:
+    def DoWork(self):
+        input_file = open('input.txt', 'r')
+        return self.GetActualSchematicSum(input_file.read().splitlines())
+
+    def GetActualSchematicSum(self, lines: [string]) -> int:
+        sum_tracker = 0
+        digit_locations = {}
+        for row, line in enumerate(lines):
+            digits = re.findall(r'\d+', line)
+            indexes = [range(m.start(0), m.end(0)) for m in re.finditer(r'\d+', line)]
+                                                                                                               
+            digit_locations[row] = [{'number': int(x), 'location': indexes[index]} for index, x in enumerate(digits)]
+
+        for row, line in enumerate(lines):
+            for col, char in enumerate(line):
+                if char == "*":
+                    close_nums = []
+                    close_nums_before = [x['number'] for x in digit_locations[row-1] if (col - 1) in x['location'] or col in x['location'] or (col + 1) in x['location']]
+                    close_nums_on = [x['number'] for x in digit_locations[row] if (col - 1) in x['location'] or col in x['location'] or (col + 1) in x['location']]
+                    close_nums_after = [x['number'] for x in digit_locations[row+1] if (col - 1) in x['location'] or col in x['location'] or (col + 1) in x['location']]
+                    
+                    close_nums.append([*close_nums_before, *close_nums_on, *close_nums_after])
+
+                    if(len(close_nums[0]) == 2):
+                        sum_tracker += math.prod([int(x) for x in close_nums[0]])
+
+        return sum_tracker
+
+
 print(PartOne().DoWork())
+print(PartTwo().DoWork())
