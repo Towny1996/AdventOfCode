157c223b216a0c2dd2f63b991f85bf5cdb79ef8b
arron.townshend@hotmail.co.uk <arron.townshend@hotmail.co.uk>
2023-12-04 19:34:50 +0000

    feat(03-12-2023): Day 3 Part 1 Solution

--- 2023/03/03_Solution.py
+++ 2023/03/03_Solution.py
@@ -2,7 +2,7 @@ import string
 import re
 
 class PartOne:
-    special_chars = ['*', '&', '#', '/', '+', '$', '%', '@', '=']
+    special_chars = ['*', '&', '#', '/', '+', '$', '%', '@', '-', '=']
     def DoWork(self) -> int:
         input_file = open('input.txt', 'r')
         return self.GetSchematicSum(input_file.read().splitlines())
@@ -12,20 +12,20 @@ class PartOne:
         for row, line in enumerate(lines):
             digits = re.findall(r'\d+', line)
             indexes = [range(m.start(0), m.end(0)) for m in re.finditer(r'\d+', line)]
-            digit_locations[row] = { 'numbers': digits, 'locations': indexes }
+
+            digit_locations[row] = [{'number': int(x), 'location': indexes[index]} for index, x in enumerate(digits)]
 
         close_nums = []
 
         for row, line in enumerate(lines):
             for col, char in enumerate(line):
                 if char in self.special_chars:
-                    close_nums_before = list(set([int(digit_locations[row-1]['numbers'][digit_locations[row-1]['locations'].index(x)]) for x in digit_locations[row-1]['locations'] for o in list(range(col-1, col+2)) if o in x]))
-                    close_nums_on = list(set([int(digit_locations[row]['numbers'][digit_locations[row]['locations'].index(x)]) for x in digit_locations[row]['locations'] for o in list(range(col-1, col+2)) if o in x]))
-                    close_nums_after = list(set([int(digit_locations[row+1]['numbers'][digit_locations[row+1]['locations'].index(x)]) for x in digit_locations[row+1]['locations'] for o in list(range(col-1, col+2)) if o in x]))
-
-                    close_nums.append([*close_nums_before, *close_nums_on, *close_nums_after])
+                    close_nums_before = [x['number'] for x in digit_locations[row-1] if col-1 in x['location'] or col in x['location'] or col+1 in x['location']]
+                    close_nums_on = [x['number'] for x in digit_locations[row] if col -1 in x['location'] or col in x['location'] or col+1 in x['location']]
+                    close_nums_after = [x['number'] for x in digit_locations[row+1] if col -1 in x['location'] or col in x['location'] or col+1 in x['location']]
                     
-        return (sum([sum(x) for x in close_nums if len(x) > 0]))
+                    close_nums.append([*close_nums_before, *close_nums_on, *close_nums_after])
 
+        return sum([sum(x) for x in close_nums])
 
 print(PartOne().DoWork())
