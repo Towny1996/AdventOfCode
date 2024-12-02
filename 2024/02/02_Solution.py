eb3c1a56e11435b95446c47234083a07c080e573
Arron <arron.townshend@hotmail.co.uk>
2024-12-02 19:31:00 +0000

    feat (2024/02): Day 2 Part 1 & 2 Solution

---
+++ 2024/02/02_Solution.py
@@ -0,0 +1,30 @@
+﻿from collections import deque
+
+from Report import Report
+
+def GetInput() -> [Report]:
+    reports = []
+    for line in open('Input.txt', 'r').readlines():
+        reports.append(Report([int(x) for x in line.split(' ')]))
+    return reports
+
+class PartOne:
+    def DoWork(self) -> int:
+        valid_reports = 0
+        for report in GetInput():
+            if report.CheckValidReport():
+                valid_reports += 1
+
+        return valid_reports
+
+class PartTwo:
+    def DoWork(self) -> int:
+        valid_reports = 0
+        for report in GetInput():
+            if report.CheckValidReport_Task_Two(0, report.levels.copy()):
+                valid_reports += 1
+
+        return valid_reports
+
+print(PartOne().DoWork())
+print(PartTwo().DoWork())
---
+++ 2024/02/Report.py
@@ -0,0 +1,43 @@
+﻿class Report(object):
+    levels = []
+    direction = string = ''
+
+    def __init__(self, levels):
+        self.levels = levels
+        self.direction = self.GetDirection(self.levels[0], self.levels[1])
+
+    ## Preserve method for part_one ##
+    def CheckValidReport(self) -> bool:
+        for index, level in enumerate(self.levels):
+            if index >= (len(self.levels) - 1):
+                continue
+
+            if not self.GetIsValidStep(level, self.levels[index + 1]):
+                return False
+
+        return True
+
+    ## Altered bruteforce for part_two ##
+    def CheckValidReport_Task_Two(self, index_popped: int, array: [int]) -> bool:
+        self.direction = self.GetDirection(array[0], array[1])
+        for index, level in enumerate(array):
+            if index >= (len(array) - 1):
+                continue
+
+            if not self.GetIsValidStep(level, array[index + 1]):
+                if index_popped >= (len(array) + 1):
+                    return False
+
+                new_arr = self.levels.copy()
+                new_arr.pop(index_popped)
+                return self.CheckValidReport_Task_Two(index_popped + 1, new_arr)
+
+        return True
+
+    def GetDirection(self, start: int, end: int) -> string:
+        return 'up' if start < end else 'down'
+
+    def GetIsValidStep(self, start: int, end: int) -> bool:
+        if (abs(start - end) < 1 or abs(start - end) > 3) or self.GetDirection(start, end) is not self.direction:
+            return False
+        return True
