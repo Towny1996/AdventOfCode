021277491a4d131ac40930d3a6820603e4ef54fe
Arron Townshend <arron.townshend@accesspay.com>
2025-12-01 19:43:26 +0000

    feat (2025/01): Day 1 solution

--- 2024/06/part_one.py
+++ 2024/06/part_one.py
@@ -1,7 +1,7 @@
 ﻿class PartOne:
-    input, obstacles, maze_size, moves = [], [], [], 0
+    input, maze_size, moves = [], [], 0
     directions = [[0, -1], [1, 0], [0, 1], [-1, 0]]
-    visited = []
+    visited, obstacles = set(), set()
 
     def __init__(self, input: []):
       self.input = input
@@ -13,7 +13,7 @@
         for y, line in enumerate(self.input):
             for x, char in enumerate(line):
                 if char == '#':
-                    self.obstacles.append([x, y])
+                    self.obstacles.add((x, y))
                 if char == '^':
                     guards_position = [x, y]
 
@@ -23,13 +23,13 @@
         guards_new_x = (guards_position[0] + self.directions[direction][0])
         guards_new_y = (guards_position[1] + self.directions[direction][1])
 
-        if any(obstacle == [guards_new_x, guards_new_y] for obstacle in self.obstacles):
+        if (guards_new_x, guards_new_y) in self.obstacles:
             direction = 0 if direction == 3 else direction + 1
             return self.MoveGuardsPosition(guards_position, direction)
 
-        if not any(visited == [guards_new_x, guards_new_y] for visited in self.visited):
+        if (guards_new_x, guards_new_y) not in self.visited:
             self.moves += 1
-            self.visited.append([guards_new_x, guards_new_y])
+            self.visited.add((guards_new_x, guards_new_y))
 
         if (guards_new_x == self.maze_size[0] or guards_new_y == self.maze_size[1]) or (guards_new_x == -1 or guards_new_y == -1):
             return self.moves
---
+++ 2025/01/01_Solution.py
@@ -0,0 +1,43 @@
+﻿import math
+
+def GetInput():
+    return open('Input.txt', 'r').readlines()
+
+def Solution(count_passes):
+    input = GetInput()
+    dial = 50
+    zeros = 0
+
+    for i in input:
+        direction = i[0]
+        turns = int(i[1:])
+
+        if turns > 99:
+            if count_passes:
+                zeros = zeros + math.floor((turns/100))
+            turns = turns % 100
+
+        match direction:
+            case 'R':
+                if dial + turns > 99:
+                    dial = (dial + turns) - 100
+                    if count_passes and dial != 0:
+                        zeros = zeros + 1
+                else:
+                    dial = dial + turns
+            case 'L':
+                if dial - turns < 0:
+                    if count_passes and dial != 0:
+                        zeros = zeros + 1
+                    dial = 100 - abs(dial - turns)
+
+                else:
+                    dial = dial - turns
+
+        if dial == 0:
+            zeros = zeros + 1
+
+    return zeros
+
+print(Solution(False))
+print(Solution(True))
