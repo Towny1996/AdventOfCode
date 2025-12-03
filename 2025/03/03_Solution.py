81a3703defc3d28483e8e4cbcfe1d2ffcf8dbcba
Arron Townshend <arron.townshend@accesspay.com>
2025-12-03 09:48:38 +0000

    feat (2025/03): Day 3 Part 1 & 2 Solution

---
+++ 2025/03/03_Solution.py
@@ -0,0 +1,41 @@
+﻿import heapq
+
+def GetInput():
+    return open('Input.txt', 'r').readlines()
+
+def Part_One() -> int:
+    valid_voltages = []
+    for line in [x.rstrip() for x in GetInput()]:
+        nums = [int(line[i:i + 1]) for i in range(0, len(line), 1)]
+
+        highest = max(nums)
+        if nums.index(highest) == (len(nums) - 1):
+            highest = heapq.nlargest(2, nums)[1]
+        second = max(nums[nums.index(highest) + 1:])
+
+        valid_voltages.append(str(highest) + str(second))
+
+    return sum([int(x) for x in valid_voltages])
+
+class Part_Two():
+    def DoWorK(self) -> int:
+        valid_voltages = []
+        for line in [x.rstrip() for x in GetInput()]:
+            nums = [int(line[i:i + 1]) for i in range(0, len(line), 1)]
+            valid_voltages.append(self.GetHighestSequence(max(nums), nums, '', 11, 1))
+
+        return sum([int(x) for x in valid_voltages])
+
+    def GetHighestSequence(self, highest, nums, found, wanted, index) -> []:
+        if abs(nums.index(highest) - (len(nums) - 1)) < ((wanted - 1) - (len(found) - 1)):
+            return self.GetHighestSequence(heapq.nlargest(index + 1, nums)[index], nums, found, wanted, index + 1)
+
+        found = found + str(highest)
+        if len(found) - 1 == wanted:
+            return found
+
+        return self.GetHighestSequence(max(nums[nums.index(highest) + 1:]), nums[nums.index(highest) + 1:], found, wanted, 1)
+
+
+#print(Part_One())
+print(Part_Two().DoWorK())
